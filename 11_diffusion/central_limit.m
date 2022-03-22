67792e9737cfa782479b68fe8724f5eb79ba6ef0
Yue Sun <yuesun@g.harvard.edu>
2022-03-22 11:30:24 -0400

    Fix error in central limit example

--- 11_diffusion/central_limit.m
+++ 11_diffusion/central_limit.m
@@ -2,16 +2,17 @@
 
 %define probability for a single process
 clf
-s=[1/3 1/3 1/3];  %uniform distribution
-% s=[1/10 2/5 1/2]; % a more weird distribution
+s=[1/3 1/3 1/3];  %uniform distribution%
+%s=[1/10 2/5 1/2]; % a more weird distribution
 
-N=40; %number of positive bins
+
+nT=10000; %number of trials
+
+nP=1;%number of processes (or steps) to sum over
+
+N=nP; %number of positive bins
 p=zeros(2*N+1,1);
 x=(1:2*N+1)-N-1;
-
-nT=50; %number of trials
-
-nP=50;%number of processes
 
 for i=1:nT;
     %compute the sum of nP i.i.d processes
@@ -26,9 +27,9 @@ for i=1:nT;
             v=v+1;
         end
     end
-    if(N+1+v>0&N+1+v<2*N+1)
+    if(N+1+v>0&N+1+v<=2*N+1) % make sure index is not out of bound
         p(N+1+v)=p(N+1+v)+1;
     end
 end
 
-bar(x,p/max(p));axis tight;grid on;
+bar(x,p/sum(p));axis tight;grid on;
